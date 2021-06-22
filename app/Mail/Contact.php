5f3b9696e312354c6d893403191657188d19e44c
Jules Thomas D <48414478+julowayne@users.noreply.github.com>
2021-06-22 23:04:55 +0200

    deleted $ from compact user

--- app/Mail/Contact.php
+++ app/Mail/Contact.php
@@ -36,6 +36,6 @@ class Contact extends Mailable
             'object' => $request->get('object'),
             'message' => $request->get('message'),
         ];
-        return $this->view('emails.contact', compact('$user'));     
+        return $this->view('emails.contact', compact('user'));     
     }
 }
